335011f59a906ea631c5f181693735c20a7277ca
Kevin-Molina <kevin.molinah@yahoo.com>
2017-08-23 16:42:20 -0700

    Synchronize versions of Bootstrap
    Use same version of Bootstrap for both css and js files

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 // On Extension Icon click, inject script
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(tab.id, {file: "content_script.js"});
+	chrome.tabs.executeScript(tab.id, {file: "jquery-3.2.1.min.js"}, function() {
+		chrome.tabs.executeScript(tab.id, {file: "bootstrap.min.js"}, function() {
+			chrome.tabs.executeScript(tab.id, {file: "content_script.js"});
+		});
+	});
 });
 
